3e42fdc3113833cd397f1000c0b77b334fe44069
BigPawTate <116862308+BigPawTate@users.noreply.github.com>
2023-10-26 11:08:00 -0700

    Create paredown.py

---
+++ paredown.py
@@ -0,0 +1,42 @@
+import os
+import shutil
+from datetime import datetime
+
+# Prompt the user for the source directory
+source_directory = input("Enter the source directory path: ")
+
+# Check if the entered directory exists
+if not os.path.exists(source_directory) or not os.path.isdir(source_directory):
+    print("The specified directory does not exist.")
+else:
+    # Prompt the user for the increment
+    try:
+        increment = int(input("Enter the increment (e.g., 1 for every file, 2 for every other file): "))
+    except ValueError:
+        print("Invalid increment. Please enter a positive integer.")
+        exit()
+
+    if increment <= 0:
+        print("Increment should be a positive integer.")
+        exit()
+
+    # Generate a timestamp
+    timestamp = datetime.now().strftime("%d%b%Y.%H%M%S")
+
+    # Create the destination subdirectory
+    destination_directory = os.path.join(source_directory, timestamp)
+    os.makedirs(destination_directory, exist_ok=True)
+
+    # Get a list of all files in the source directory
+    files = [f for f in os.listdir(source_directory) if os.path.isfile(os.path.join(source_directory, f))]
+
+    # Create a list of files to copy based on the specified increment
+    files_to_copy = files[::increment]
+
+    # Copy the selected files to the destination subdirectory
+    for file_name in files_to_copy:
+        source_path = os.path.join(source_directory, file_name)
+        destination_path = os.path.join(destination_directory, file_name)
+        shutil.copy2(source_path, destination_path)
+
+    print(f"Copied {len(files_to_copy)} files with an increment of {increment} to {destination_directory}")
