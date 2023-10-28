cef7e395cdd387fec166baf6f7d08a8c921527c6
BigPawTate <116862308+BigPawTate@users.noreply.github.com>
2023-10-28 14:21:34 -0700

    Update paredown.py

--- paredown.py
+++ paredown.py
@@ -20,18 +20,22 @@ def has_existing_timestamp(filename):
     return re.search(timestamp_pattern, filename) is not None
 
 # Prompt the user for the directory path where picture files are located
-source_directory = input("Enter the directory path where your picture files are located: ")
+source_directory = input("\nProvide the directory where your images are located:\n")
 
 # Check if the provided path is a valid directory
 if not os.path.isdir(source_directory):
-    print("Invalid directory path. Please provide a valid directory path.")
+    print("\nInvalid directory path. Please provide a valid directory path.")
     exit()
 
-# Prompt the user for the increment of files they want to keep
-increment = int(input("Enter the increment (e.g., 1 for every file, 2 for every other file): "))
+# Count the number of image files in the source directory
+image_files = [filename for filename in os.listdir(source_directory) if filename.lower().endswith(('.jpg', '.jpeg', '.png', '.gif'))]
+num_image_files = len(image_files)
+
+# Prompt for the increment based on the number of image files
+increment = int(input(f"\n {num_image_files} images in directory. \n\nEnter the increment (e.g., 1 for every file, 2 for every other file): \n"))
 
 # Prompt the user for the output directory or press Enter to use the default
-output_directory = input("Enter the output directory path (or press Enter to use default timestamp directory): ")
+output_directory = input("\nProvide the output directory path (or press Enter):\n")
 
 # Create a subdirectory with the current timestamp (with space between day and hour)
 timestamp = datetime.now().strftime('%Y%m%d %H%M%S')
@@ -89,6 +93,6 @@ for filename in os.listdir(source_directory):
             print(f"No EXIF data found for '{filename}'")
 
 if not output_directory:
-    print(f"{copied_files_count} copied files to: {subdirectory}")
+    print(f"\n{copied_files_count} files copied to: {subdirectory}")
 else:
-    print(f"{copied_files_count} copied files to: {output_directory}")
+    print(f"\n{copied_files_count} files copied to: {output_directory}")
